c01ef21ae164278f109be562fdf17e4554b52652
Andrea Guffi <andreaguffi92@gmail.com>
2023-06-21 15:37:50 +0200

    installato vari packages

--- lib/components/buttons/ButtonSecondary.dart
+++ lib/components/buttons/ButtonSecondary.dart
@@ -10,14 +10,14 @@ class ButtonSecondary extends StatelessWidget {
   final double width;
   final double height;
 
-  const ButtonSecondary(
-      {Key? key,
-      this.text = "Accedi",
-      this.width = 100,
-      this.height = 56,
-      this.textColor = ConstantsUI.PRIMARY_TEXT_COLOR,
-      this.backgroundColor = ConstantsUI.PRIMARY_BACKGROUND})
-      : super(key: key);
+  const ButtonSecondary({
+    Key? key,
+    this.text = "Accedi",
+    this.width = 100,
+    this.height = 56,
+    this.textColor = ConstantsUI.PRIMARY_TEXT_COLOR,
+    this.backgroundColor = ConstantsUI.PRIMARY_BACKGROUND,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
