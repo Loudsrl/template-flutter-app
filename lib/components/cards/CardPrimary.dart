ddef7b465bdea35eb10b64f8f27cab62162f7ce9
Andrea Merli <andrea@loudsrl.com>
2023-06-21 12:42:26 +0200

    add card primary, loading

---
+++ lib/components/cards/CardPrimary.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CardPrimary extends StatefulWidget {
+  final double cardHeight;
+  final double cardWidth;
+  final BorderRadius borderRadius;
+  final List<BoxShadow> listShadow;
+  final Widget contentWidget;
+  final Color backgroundColor;
+  final BoxBorder boxBorder;
+
+  const CardPrimary({
+    Key? key,
+    required this.cardHeight,
+    required this.cardWidth,
+    required this.contentWidget,
+    required this.borderRadius,
+    required this.listShadow,
+    required this.backgroundColor,
+    required this.boxBorder,
+  }) : super(key: key);
+  _CardPrimaryState createState() => new _CardPrimaryState();
+}
+
+class _CardPrimaryState extends State<CardPrimary> {
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.cardHeight,
+      width: widget.cardWidth,
+      decoration: BoxDecoration(
+        borderRadius: widget.borderRadius,
+        boxShadow: widget.listShadow,
+        color: widget.backgroundColor,
+        border: widget.boxBorder,
+      ),
+      child: widget.contentWidget,
+    );
+  }
+}
---
+++ lib/components/loading/Loading.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  final String? text;
+  final Color? textColor;
+  final EdgeInsets? textPadding;
+
+  final MainAxisAlignment align;
+  final Color loadingColor;
+  final double loadingSize;
+  final EdgeInsets? loadingPadding;
+
+  Loading({
+    Key? key,
+    this.text,
+    this.textColor,
+    required this.align,
+    required this.loadingColor,
+    required this.loadingSize,
+    this.loadingPadding,
+    this.textPadding,
+  }) : super(key: key);
+  _LoadingState createState() => new _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.loadingSize + 20,
+      width: widget.loadingSize,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: widget.align,
+        children: [
+          Container(
+            height: widget.loadingSize,
+            width: widget.loadingSize,
+            padding: widget.loadingPadding != null
+                ? widget.loadingPadding!
+                : const EdgeInsets.all(20),
+            child: CircularProgressIndicator(
+              color: widget.loadingColor,
+            ),
+          ),
+          widget.text != null
+              ? Padding(
+                  padding: widget.textPadding != null
+                      ? widget.textPadding!
+                      : const EdgeInsets.only(top: 8),
+                  child: Text(
+                    widget.text!,
+                    style: TextStyle(color: widget.textColor),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
