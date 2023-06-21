e9f4dfe35313d70cab677029ed587a24fb8d32d2
Andrea Guffi <andreaguffi92@gmail.com>
2023-06-21 15:48:32 +0200

    spalsh page

---
+++ lib/pages/SplashPage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'SplashPage.model.dart';
+
+class SplashPageView extends StatelessWidget {
+  const SplashPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<SplashPageModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        body: Container(
+          child: Container(
+            color: Colors.white,
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  height: 50,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 15),
+                  height: 50,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text('Caricamento'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      onModelReady: (model) {
+        model.init(context);
+      },
+      viewModelBuilder: () => SplashPageModel(),
+    );
+  }
+}
---
+++ lib/pages/SplashPage.model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+class SplashPageModel extends BaseViewModel {
+  void init(BuildContext context) async {}
+}
